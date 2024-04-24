9609adb507285453d25980a3cd4f7185610ab158
闫茂源 <yanmaoyuan@baijiayun.com>
2024-04-24 23:56:22 +0800

    feat: support better animation control

---
+++ demo/src/main/java/io/github/jmecn/tiled/demo/Const.java
@@ -0,0 +1,7 @@
+package io.github.jmecn.tiled.demo;/**
+ * desc: 
+ * 
+ * @author yanmaoyuan
+ * @date 2024/4/24
+ */public interface Const {
+}
---
+++ demo/src/main/java/io/github/jmecn/tiled/demo/control/AnimStateControl.java
@@ -0,0 +1,7 @@
+package io.github.jmecn.tiled.demo.control;/**
+ * desc: 
+ * 
+ * @author yanmaoyuan
+ * @date 2024/4/24
+ */public class AnimStateControl {
+}
