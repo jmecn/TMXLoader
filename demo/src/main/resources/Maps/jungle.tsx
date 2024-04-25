5e5d31bd3896c2eed6af804db94db3ff29ac12d2
闫茂源 <yanmaoyuan@baijiayun.com>
2024-04-25 11:23:53 +0800

    feat: update demo map tileset

---
+++ demo/src/main/resources/Maps/jungle.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="jungle" tilewidth="16" tileheight="16" tilecount="462" columns="22">
+  <image source="tf_jungle_tileset.png" width="352" height="336"/>
+</tileset>
---
+++ demo/src/main/resources/Maps/tree.tsx
@@ -0,0 +1,51 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="objects" tilewidth="80" tileheight="80" tilecount="6" columns="0">
+  <tile id="0" x="160" y="80" width="80" height="80">
+   <image width="352" height="336" source="tf_jungle_tileset.png"/>
+   <objectgroup draworder="index" id="2">
+    <object id="9" x="1.20968" y="0.806452" width="76.6129" height="68.5484"/>
+   </objectgroup>
+  </tile>
+  <tile id="1" x="240" y="80" width="80" height="80">
+   <image width="352" height="336" source="tf_jungle_tileset.png"/>
+   <objectgroup draworder="index" id="2">
+    <object id="4" x="1.20968" y="6.04839" width="77.0161" height="64.9194"/>
+   </objectgroup>
+  </tile>
+  <tile id="2" x="240" y="160" width="80" height="16">
+   <image width="352" height="336" source="tf_jungle_tileset.png"/>
+   <objectgroup draworder="index" id="2">
+    <object id="1" x="17" y="0" width="46" height="14">
+     <properties>
+      <property name="body_type" value="static"/>
+     </properties>
+    </object>
+   </objectgroup>
+  </tile>
+  <tile id="3" x="16" y="304" width="32" height="32">
+   <image width="352" height="336" source="tf_jungle_tileset.png"/>
+   <objectgroup draworder="index" id="2">
+    <object id="2" x="2.01613" y="15.7258" width="27.0161" height="12.0968">
+     <properties>
+      <property name="body_type" value="static"/>
+     </properties>
+    </object>
+   </objectgroup>
+  </tile>
+  <tile id="4" x="48" y="304" width="32" height="16">
+   <image width="352" height="336" source="tf_jungle_tileset.png"/>
+   <objectgroup draworder="index" id="2">
+    <object id="2" x="3" y="1" width="26" height="14">
+     <properties>
+      <property name="body_type" value="static"/>
+     </properties>
+    </object>
+   </objectgroup>
+  </tile>
+  <tile id="6" x="80" y="304" width="16" height="16">
+   <image width="352" height="336" source="tf_jungle_tileset.png"/>
+   <objectgroup draworder="index" id="2">
+    <object id="1" x="0.806452" y="2.01613" width="14.5161" height="12.5"/>
+   </objectgroup>
+  </tile>
+</tileset>
